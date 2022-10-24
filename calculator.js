f9ab4d8f79679a9a83ed85be88f0a2ecc45a8eb9
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-10-24 15:52:59 +0700

    added new param and new conditional

--- calculator.js
+++ calculator.js
@@ -1,24 +1,42 @@
-function priceCalculator(book, discount, tax, stock, order){
-    let result = 0;
-    if(book && book.price){
-        const price=book.price;
-        const discountAmount=price*(discount/100);
-        const priceAfterDiscount=price - discountAmount;
-        const taxAmount=priceAfterDiscount*(tax/100);
-        const priceAfterTax=priceAfterDiscount+taxAmount;
+function priceCalculator(book, discount, tax, stock, order) {
+  let totalPriceToPay = 0;
+  const price = book.price;
+  const discountAmount = price * (discount / 100);
+  const priceAfterDiscount = price - discountAmount;
+  const taxAmount = priceAfterDiscount * (tax / 100);
+  const priceAfterTax = priceAfterDiscount + taxAmount;
+  let totalBooksPrice = 0;
+  let totalPriceAfterDiscount = 0;
+  let totalTax = 0;
 
-        console.group();
-        console.log("Judul Buku : ", book.title);
-        console.log("Harga Buku : ",price);
-        console.log("Persentase diskon : ",discount,"%");
-        console.log("Biaya setelah diskon : ",priceAfterDiscount);
-        console.log("Persantase PPN : ",tax,"%");
-        console.log("Biaya PPN : ",taxAmount);
-        console.log("Total pembayaran : ",priceAfterTax);
-        console.groupEnd();
+  for (let index = 0; index < order; index++) {
+    if (book && book.price && index < stock) {
+      totalPriceAfterDiscount += priceAfterDiscount;
+      totalBooksPrice += book.price;
+      totalTax += taxAmount;
+    } else {
+      console.log("Stock run out!");
+      break;
     }
-
-    return 0;
+  }
+  totalPriceToPay = totalPriceAfterDiscount + totalTax;
+  if (book && book.price && order <= stock) {
+    console.group();
+    console.log("Judul Buku : ", book.title);
+    console.log("Harga Satuan : ", price);
+    console.log("Jumlah yang dibeli : ", order);
+    console.log("Total harga keseluruhan : ", totalBooksPrice);
+    console.log("Persentase diskon : ", discount, "%");
+    console.log("Biaya setelah diskon : ", totalPriceAfterDiscount);
+    console.log("Persantase PPN : ", tax, "%");
+    console.log("Biaya PPN : ", totalTax);
+    console.log("Total pembayaran : ", totalPriceToPay);
+    if (stock - order === 0)
+      console.log("Stok habis, anda tidak bisa membeli lagi");
+    else console.log("Stok masih ada, anda bisa membeli lagi");
+    console.groupEnd();
+  }
+  return 0;
 }
 
-priceCalculator({title: "JS For baby", price: 125000,},50,10);
+priceCalculator({ title: "JS For baby", price: 125000 }, 50, 10, 10, 9);
