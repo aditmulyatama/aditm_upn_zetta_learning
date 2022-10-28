ab00846769d89954daa26a8c71f829285f326183
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-10-28 21:31:36 +0700

    -answering the question

--- combinator.ts
+++ combinator.ts
@@ -10,3 +10,6 @@ const text1 = [1, "data", "3", "result"];
 const text2 = ["Bejo", "has", "4", "sport", "car"];
 console.log(combinator(text1));
 console.log(combinator(text2));
+//array digunakan jika ingin menyimpan beberapa data dengan tipe data yang sama ke dalam 1 variabel
+//tuple digunakan jika ingin menyimpan beberapa data dengan tipe data yang berbeda ke dalam 1 variabel
+//object digunakan jika ingin menyimpan data yang data-datanya terdapat beberapa objek yang bisa didefinisikan di dalamnya
