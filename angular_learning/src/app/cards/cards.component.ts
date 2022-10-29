a594a80c295d4fadf8309d0c05cb08304055adfe
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-10-29 14:16:21 +0700

    Still learning

--- angular_learning/src/app/cards/cards.component.ts
+++ angular_learning/src/app/cards/cards.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
+  allowNewCards = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewCards = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
 }
