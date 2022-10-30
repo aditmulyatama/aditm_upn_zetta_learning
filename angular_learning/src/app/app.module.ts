574e387e8c394ab01f626dfc17cdf3843aa6ddcf
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-10-30 15:52:27 +0700

    day 2 done... hopefully

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
-
+import { FormsModule } from '@angular/forms';
+import { AddServerCardsComponent } from './add-server-cards/add-server-cards.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardsComponent, AddServerCardsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
