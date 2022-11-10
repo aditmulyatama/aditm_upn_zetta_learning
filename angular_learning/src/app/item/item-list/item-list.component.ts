fde62ccd3d5ac6be089fefff8799b7b0a975d5ac
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-11-10 20:36:47 +0700

    -add items in item-list

--- angular_learning/src/app/item/item-list/item-list.component.ts
+++ angular_learning/src/app/item/item-list/item-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ItemService } from 'src/app/services/item.service';
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.scss']
+  styleUrls: ['./item-list.component.scss'],
 })
 export class ItemListComponent implements OnInit {
+  constructor(private itemService: ItemService) {}
+  items: {
+    id: number;
+    gambar: string;
+    nama: string;
+    harga: number;
+    kondisi: string;
+    link_pembelian: string;
+  }[] = this.itemService.itemList;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
