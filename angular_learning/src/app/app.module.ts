977dd6772bd1a44cfbd6e2d2e0d1df832526c26a
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-11-11 21:47:03 +0700

    yeah

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -9,16 +9,8 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserInputComponent } from './user/user-input/user-input.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { HomeComponent } from './home/home.component';
-import { MatCardModule } from '@angular/material/card';
-import {
-  MatFormFieldModule,
-  FloatLabelType,
-} from '@angular/material/form-field';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { DataService } from 'src/app/services/data.service';
-import { MatRadioModule } from '@angular/material/radio';
 import { MaterialModule } from './module/module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -35,12 +27,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatCardModule,
-    MatFormFieldModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatRadioModule,
     MaterialModule,
     BrowserAnimationsModule,
   ],
