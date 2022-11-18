b4de65076520930e88a7e6325637e6c5b4c239d1
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-11-18 14:05:52 +0700

    -Angular Day 11

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { DataService } from './service/data.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_learning';
+
 }
